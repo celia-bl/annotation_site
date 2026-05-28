4d3ed379247102d16df82d79874ca4a986260838
Celia <blondin.celia@yahoo.fr>
2026-05-28 21:24:55 +0200

    annotation_v1

--- run.bat
+++ run.bat
@@ -1,4 +1,10 @@
 @echo off
-start uvicorn main:app --reload
+echo Download Github modifs
+git fetch origin
+git reset --hard origin/master
+echo App Launching
+call annotation-venv\Scripts\activate
+start "Backend" cmd /k "cd backend && uvicorn main:app --reload"
+start "Frontend" cmd /k "cd frontend && npm run dev"
 timeout /t 2
-start npm run dev
+start http://localhost:5173
